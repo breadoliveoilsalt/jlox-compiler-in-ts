683070d63545dc0051b93f451896ae19de7831b6
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2024-08-05 16:23:21 -0400

    Remove build from git

--- build/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vitest_1 = require("vitest");
-(0, vitest_1.describe)('passing test', function () {
-    (0, vitest_1.test)('it passes', function () {
-        (0, vitest_1.expect)(true).toEqual(true);
-    });
-});
