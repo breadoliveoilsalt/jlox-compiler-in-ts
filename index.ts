e670d584d976472c2ec63b4b050ccb647faa8b50
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2024-08-07 19:05:48 -0400

    Add file reader that simulates reading line line at a time

--- index.ts
+++ index.ts
@@ -1,16 +1,74 @@
-import { open } from 'node:fs/promises';
+// import { open } from 'node:fs/promises';
+// import lineByLine from 'n-readline';
+import { scan } from './scanner';
+import * as fs from 'node:fs';
 
-async function main() {
-  try {
-    console.log('\n----- compiling -----\n');
-    const file = await open('./src.jlox');
+async function initLineReader({ filePath }: { filePath: string }) {
+  const file = fs.readFileSync(filePath, 'utf8');
+  const data = file.split('\n')
 
-    for await (const line of file.readLines()) {
-      console.log(line);
-    }
-  } catch (e) {
-    console.log(e);
+  async function readLine() {
+    if (data.length === 0) return Promise.resolve(false)
+    return Promise.resolve(data.shift())
+  }
+
+  return {
+    readLine
   }
 }
+// const lineReader = new reader('./src.jlox');
+// console.log(lineReader);
+// console.log(lineReader.next())
 
-main()
+// const fileHandle = await open('./src.jlox');
+// const readStream = fileHandle.readLines();
+// console.log(readStream.prototype)
+// console.log('fileHandle', fileHandle)
+// console.log('typeof', typeof fileHandle)
+// console.log('readline', readStream)
+// console.log('typeof', typeof readStream)
+
+// readStream.question('how is it going?', true, true)
+// const thing = readStream.resume();
+// console.log('thing', thing)
+// readStream.close()
+
+// return async function () {
+// const line = await fileHandle.readLines();
+// console.log('line', line);
+
+// return await line.next();
+// return await fileHandle.readLines();
+// }; // return async function () {
+//   console.log('first');
+//   return Promise.resolve('yo');
+// };
+
+async function main() {
+  console.log('\n----- compiling -----\n');
+  const { readLine } = await initLineReader({ filePath: './src.jlox' });
+  const line = await readLine()
+  console.log(line)
+  // console.log(readline());
+  // scan({ readline });
+}
+// async function main() {
+//   try {
+//     console.log('\n----- compiling -----\n');
+//     // const fileHandle = await open('./src.jlox');
+//     // const { readLines } = await open('./src.jlox');
+
+//     const readline = fileHandle.readLines;
+
+//     // scan({ readline: readlines })
+//     scan({ readline });
+
+//     // for await (const line of fileHandle.readLines()) {
+//     //   console.log(line);
+//     // }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+main();
---
+++ scanner.ts
@@ -0,0 +1,130 @@
+import type { CreateReadStreamOptions } from 'node:fs/promises';
+// import type { Interface } from "node";
+const tokenTypes = [
+  {
+    name: 'leftParen',
+    test: (buffer: string) => buffer.match(/^\(/),
+  },
+  {
+    name: 'rightParen',
+    test: (buffer: string) => buffer.match(/^\)/),
+    // extract: (readline, currentBuffer) => // fill in : return token and updated buffer
+  },
+];
+
+type LineReader = (options?: CreateReadStreamOptions | undefined) => Interface;
+
+type ScanParams = {
+  // readline: () => string,
+  readline: LineReader;
+  buffer: string;
+  tokens: [];
+  line: number;
+};
+
+// TODO: see if I can get away with this
+const defaultScanArguments = {
+  readline: () => '',
+  buffer: '',
+  tokens: [],
+  line: 0,
+};
+
+export async function scan(
+  { readline, buffer, tokens, line }: ScanParams = {
+    readline: async () => Promise.resolve(''),
+    buffer: '',
+    tokens: [],
+    line: 0,
+  },
+) {
+
+
+  // const lineString = await readline()
+  // console.log(lineString)
+  // const currentLine = line + 1;
+  // const currentBuffer = buffer + readline();
+  for await (const line of readline()) {
+    console.log(line);
+  }
+  console.log('done')
+  
+}
+
+// const TOKEN_NAMES = {
+//   LEFT_PAREN: 'leftParen',
+//   RIGHT_PAREN: 'rightParen',
+//   LEFT_BRACE: 'leftBrace',
+//   RIGHT_BRACE: 'rightBrace',
+//   COMMA: 'comma',
+//   DOT: 'dot',
+//   MINUS: 'minus',
+//   PLUS: 'plus',
+//   SEMICOLON: 'semicolon',
+//   SLASH: 'slash',
+//   STAR: 'star',
+//   BANG: 'bang',
+//   BANG_EQUAL: 'bangEqual',
+//   EQUAL: 'equal',
+//   EQUAL_EQUAL: 'equalEqual',
+//   GREATER: 'greater',
+//   GREATER_EQUAL: 'greaterEqual',
+//   LESS: 'less',
+//   LESS_EQUAL: 'lessEqual',
+//   IDENTIFIER: 'identifier',
+//   STRING: 'string',
+//   NUMBER: 'number',
+//   AND: 'and',
+//   CLASS: 'class',
+//   ELSE: 'else',
+//   FALSE: 'false',
+//   FUN: 'fun',
+//   FOR: 'for',
+//   IT: 'it',
+//   NIL: 'nil',
+//   OR: 'or',
+//   PRINT: 'print',
+//   RETURN: 'return',
+//   SUPER: 'super',
+//   THIS: 'this',
+//   TRUE: 'true',
+//   VAR: 'var',
+//   WHILE: 'while',
+//   EOF: 'eof',
+// };
+
+// const TOKEN_REGEXES: Array<[TokenType, RegExp]> = [
+//   [TOKEN_NAMES.BANG, new RegExp(/\b!/)],
+//   [TOKEN_NAMES.TRUE, new RegExp(/true/)],
+//   [TOKEN_NAMES.FALSE, new RegExp(/false/)],
+// ];
+
+// export type TokenType = (typeof TOKEN_NAMES)[keyof typeof TOKEN_NAMES];
+
+// type Token = {
+//   type: TokenType;
+//   line: number;
+//   lexeme: string;
+//   // literal: Object, // TN: come back to
+// };
+
+// let line = 0;
+
+// const tokens: Array<Token> | [] = [];
+
+// export function scan(line: string) {
+//   if (line === '') return;
+//   TOKEN_REGEXES.forEach(([type, re]) => {
+//     const matches = line.match(re);
+//     // if (!matches) return;
+//     tokens.push({
+//       type,
+//       line,
+//       lexeme: matches[0],
+//     });
+//   });
+// }
+
+// export function getTokens() {
+//   return tokens;
+// }
