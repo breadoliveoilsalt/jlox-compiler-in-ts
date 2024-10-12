5065b3d877d1857335ac5003c16f4b4c2823532e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2024-10-12 18:06:14 -0400

    WIP on tests - new buildReadline method works, but no idea why tS error

---
+++ compile.test.ts
@@ -0,0 +1,264 @@
+import { describe, test, expect } from 'vitest';
+import { scan } from './scanner';
+import { parse } from './parser';
+import { type ReadLine } from './index';
+
+// TODO: Refactor b/c I can't tell which test is failing thanks
+// to assertion being elsewhere
+
+export async function compile(readLine: ReadLine) {
+  const { tokens } = await scan(readLine);
+  const parsedResults = parse({ tokens });
+  return parsedResults[parsedResults.length - 1].evaluate();
+}
+
+function buildReadLine(lines: string[]) {
+  const linesWithEOF = [...lines, false]
+  async function readLine() {
+    if (linesWithEOF.length === 0) return Promise.resolve(false)
+    return Promise.resolve(linesWithEOF.shift());
+  }
+  return readLine;
+}
+
+async function testCompiler({ line, expected }) {
+  // const lines = [line, false];
+  // async function readLine() {
+  //   return Promise.resolve(lines.shift());
+  // }
+  
+  const readLine = buildReadLine([line])
+
+  expect(await compile(readLine)).toEqual(expected);
+}
+
+
+describe('compile', () => {
+  test.each([
+    {
+      line: 'true;',
+      expected: true,
+    },
+    {
+      line: 'false;',
+      expected: false,
+    },
+    {
+      line: '!true;',
+      expected: false,
+    },
+    {
+      line: '!false;',
+      expected: true,
+    },
+    {
+      line: '!!false;',
+      expected: false,
+    },
+    {
+      line: '!!true;',
+      expected: true,
+    },
+    {
+      line: '!!!true;',
+      expected: false,
+    },
+  ])(
+    'it compiles boolean expressions, such as $line',
+    async ({ line, expected }) => {
+      await testCompiler({ line, expected });
+    },
+  );
+
+  test.each([
+    {
+      line: 'true == true;',
+      expected: true,
+    },
+    {
+      line: 'false == true;',
+      expected: false,
+    },
+    {
+      line: 'true == false;',
+      expected: false,
+    },
+    {
+      line: 'false == false;',
+      expected: true,
+    },
+    {
+      line: '!false == true;',
+      expected: true,
+    },
+    {
+      line: '!true == true;',
+      expected: false,
+    },
+    {
+      line: 'true == !false;',
+      expected: true,
+    },
+    {
+      line: 'true == !true;',
+      expected: false,
+    },
+  ])(
+    'it compiles equality expressions, such as $line;',
+    async ({ line, expected }) => {
+      await testCompiler({ line, expected });
+    },
+  );
+
+  test.each([
+    {
+      line: '(true);',
+      expected: true,
+    },
+    {
+      line: '((true));',
+      expected: true,
+    },
+    {
+      line: '(!true);',
+      expected: false,
+    },
+    {
+      line: '((!true));',
+      expected: false,
+    },
+    {
+      line: '(false);',
+      expected: false,
+    },
+    {
+      line: '(!false);',
+      expected: true,
+    },
+    {
+      line: '(false == true);',
+      expected: false,
+    },
+    {
+      line: '(!false == true);',
+      expected: true,
+    },
+    {
+      line: '(!false) == (true);',
+      expected: true,
+    },
+    {
+      line: '(false == true) == (true == false);',
+      expected: true,
+    },
+    {
+      line: '(false == true) != (true == true);',
+      expected: true,
+    },
+    {
+      line: '(true) == (false != true);',
+      expected: true,
+    },
+    {
+      line: '((!true)) != (false != true);',
+      expected: true,
+    },
+    {
+      line: '!(!true);',
+      expected: true,
+    },
+    {
+      line: '!(!true == false);',
+      expected: false,
+    },
+  ])(
+    'it compiles parenthetical expressions, such as $line',
+    async ({ line, expected }) => {
+      await testCompiler({ line, expected });
+    },
+  );
+
+  test.each([
+    {
+      line: '24 + 31;',
+      expected: 55,
+    },
+    {
+      line: '33 - 21;',
+      expected: 12,
+    },
+    {
+      line: '33 / 11;',
+      expected: 3,
+    },
+    {
+      line: '44 * 3;',
+      expected: 132,
+    },
+    {
+      line: '2 * 3 + 2;',
+      expected: 8,
+    },
+    {
+      line: '2 * (3 + 2);',
+      expected: 10,
+    },
+    {
+      line: '(2 * (3 + 2)) * 3;',
+      expected: 30,
+    },
+    {
+      line: '-13 + -7;',
+      expected: -20,
+    },
+    {
+      line: '-13 + 5;',
+      expected: -8,
+    },
+    {
+      line: '33.11 + 23;',
+      expected: 56.11,
+    },
+    {
+      line: '10.11 - 0.11;',
+      expected: 10,
+    },
+    {
+      line: '10 - 21;',
+      expected: -11,
+    },
+  ])(
+    'it compiles term, factor, and negating urnary expressions, to do basic math, such as $line',
+    async ({ line, expected }) => {
+      await testCompiler({ line, expected });
+    },
+  );
+
+  test('it permits the declaration of global variables', () => {
+    testCompiler({line: 'var thing = true;', expected: null})
+  })
+
+  test('initialized global variable are assigned a value', () => {
+    testCompiler({line: 'var thing = 14 + 2; thing;', expected: 16})
+  })
+
+  test.skip('initialized global variable can be reassigned', () => {
+    testCompiler({line: 'var thing = 14 + 2; thing = 17; thing;', expected: 17})
+  })
+
+  test('initialized global variable can be evaluated with other expressions', () => {
+    testCompiler({line: 'var thing = 14 + 2; thing + 3;', expected: 19})
+  })
+
+  test.only('assigning to an uninitialized variable throws and error', async () => {
+    const lines = [
+      'var thing = 14;',
+      'thing + 15;',
+    ]
+
+    const readLine = buildReadLine(lines)
+
+    expect(await compile(readLine)).toEqual(29)
+
+  })
+});
