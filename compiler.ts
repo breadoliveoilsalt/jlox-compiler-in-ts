f88a41e9ec5be983703d68a2249bf6a4f8828ccc
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2024-10-21 07:59:40 -0400

    WIP

--- compiler.ts
+++ compiler.ts
@@ -2,6 +2,9 @@ import { scan } from './scanner';
 import { parse } from './parser';
 import { type ReadLine } from './index';
 
+// UPTO - I think I need to return
+// parsedResult and env, and pass in env 
+// optionally for repl
 export async function compile(readLine: ReadLine) {
   const { tokens } = await scan(readLine);
   const parsedResults = parse({ tokens });
